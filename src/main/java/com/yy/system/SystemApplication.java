e36ade9c4527d141e21fd7453eb7e5ef222961d3
git <251380405@qq.com>
2021-08-16 15:52:18 +0800

    git分支测试

--- src/main/java/com/yy/system/SystemApplication.java
+++ src/main/java/com/yy/system/SystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
-// 如果不配置scanBasePackages则只扫描该工程
+// 如果不配置scanBasePackages则只扫描该工程 
 // 配置了则会扫描依赖的工程包（由于common工程里面有一个请求日志处理切面，这里要考虑到，所以包名只到yy级别）
 @SpringBootApplication(scanBasePackages = "com.yy")
 public class SystemApplication {
